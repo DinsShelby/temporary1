6837d2628d47f9331da2ad3215dc8fa4bcad2f3f
Andy Prasetya <54360298+nullptr03@users.noreply.github.com>
2023-07-05 17:09:58 +0800

    AlphaDroid init

--- build_rom.sh
+++ build_rom.sh
@@ -1,17 +1,16 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/alphadroid-project/manifest -b alpha-13 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/nullptr03/local_manifest.git --depth 1 -b cherish .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # ghay cache wen stop f
 source build/envsetup.sh
-lunch cherish_merlinx-userdebug
+lunch lineage_merlinx-userdebug
 export TZ=Asia/Makassar
 export BUILD_USERNAME=Andy
 export BUILD_HOSTNAME=Andy
 export KBUILD_USERNAME=Andy
 export KBUILD_HOSTNAME=Andy
-export CHERISH_VANILLA=true
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
